2c7705295217fffff8ee5d808612e215d14d8eb9
vbuell <vbuell@vbuell-zl.(none)>
2012-12-05 15:50:24 +0200

    Initial commit

---
+++ torram.py
@@ -0,0 +1,164 @@
+#!/usr/bin/python
+import sys, os, hashlib, StringIO, bencode
+
+DELUGE_DIR = '~/.config/deluge/state'
+FILES_DIR = '~'
+MINIMUM_FILESIZE_TO_SEARCH = 1024 * 256
+DO_NOT_SHOW_SKIPPED = True
+
+
+def get_file_sizes(info):
+    global args
+    if 'files' in info: # yield pieces from a multi-file torrent
+        return [fi['length'] for fi in info['files'] if fi['length'] > int(args.minsize)]
+    else: # yield pieces from a single file torrent
+        return [info['length']]
+
+
+def get_possible_files(rootdir, sizes):
+    """Find duplicate files in directory tree."""
+    filesizes = {}
+    # Build up dict with key as filesize and value is list of filenames.
+    for path, dirs, files in os.walk(rootdir):
+        for filename in files:
+            filepath = os.path.join(path, filename)
+            filesize = os.lstat(filepath).st_size
+            if filesize in sizes:
+                filesizes.setdefault(filesize, []).append(filepath)
+    return filesizes
+
+
+def check_file_chunk(hash, offset, length, file):
+    sfile = open(file.decode('UTF-8'), "rb")
+
+    sfile.seek(offset)
+    piece = sfile.read(length)
+
+    piece_hash = hashlib.sha1(piece).digest()
+    return piece_hash == hash
+
+
+def get_chunk(filesizes, global_offset):
+    """
+    Returns file offset using global continuous file
+
+    >>> get_chunk([], 0)
+    (0, 0)
+    >>> get_chunk([100], 0)
+    (0, 0)
+    >>> get_chunk([100], 100)
+    (0, 0)
+    >>> get_chunk([50,50,30], 100)
+    (2, 0)
+    """
+    file_offset = 0
+    file_idx = 0
+
+    # Find file idx
+    for filesize in filesizes:
+        if file_offset + filesize > global_offset:
+            break
+
+        file_offset += filesize
+        file_idx += 1
+
+    return (file_idx, global_offset - file_offset)
+
+
+def get_similatity_rate(success_blocks, all_blocks):
+    if all_blocks == 0:
+        return 0
+    rate = success_blocks / all_blocks
+    if rate > 0.9: return
+
+def guess_file(file_info, file_idx, files, pieces, piece_length, files_sizes_array):
+    global args
+    global fmt
+    if 'path' in file_info:
+        file_name = os.path.join(*file_info['path'])
+    else:
+        file_name = file_info['name']
+
+    file_length = file_info['length']
+
+    if file_length in files:
+        print "Processing file:", file_name
+        for file in files[file_length]:
+            pieces.seek(0)
+            sys.stdout.write(" * " + str(file))
+
+            num_of_checks = 0
+            num_of_successes = 0
+            offset = 0
+            while True:
+                hash = pieces.read(20)
+                if not hash:
+                    break
+                idx, file_offset = get_chunk(files_sizes_array, offset * piece_length)
+
+                if idx == file_idx:
+                    num_of_checks += 1
+                    if check_file_chunk(hash, file_offset, piece_length, file):
+                        num_of_successes += 1
+
+                offset += 1
+
+            print "\t[", num_of_successes, "of", num_of_checks, ']'
+    else:
+        if args.verbose > 0:
+            print "Skipping file:", file_name
+
+
+def main():
+    global args
+
+    # Open torrent file
+    torrent_file = open(args.torrentfile, "rb")
+    metainfo = bencode.bdecode(torrent_file.read())
+    info = metainfo['info']
+    pieces = StringIO.StringIO(info['pieces'])
+
+    sizes = get_file_sizes(info)
+    print 'Sizes', sizes
+
+    print 'Searching for file pretenders...'
+
+    # Get possible files
+    files = get_possible_files(os.path.expanduser(args.root), sizes)
+
+    # Check files one by one
+    if 'files' in info:
+        files_sizes_array = [f['length'] for f in info['files']]
+        file_idx = 0
+        for f in info['files']:
+            guess_file(f, file_idx, files, pieces, info['piece length'], files_sizes_array)
+            file_idx += 1
+    else:
+        print info.keys()
+        files_sizes_array = [info['length']]
+        guess_file(info, 0, files, pieces, info['piece length'], files_sizes_array)
+
+
+if __name__ == "__main__":
+#    import doctest
+#    doctest.testmod()
+
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser(description='Tries to find files used in torrent and generate download folder using them.')
+    parser.add_argument('torrentfile', metavar='torrentFile', help='File to analyze')
+    parser.add_argument('root', metavar='root', help='Dir to search')
+    parser.add_argument('--symlink', action='store_true',
+        help='Use symlinks instead of copying files. (Caution: You may lost data if files actually are not the same.)')
+    parser.add_argument('--minsize', default=MINIMUM_FILESIZE_TO_SEARCH)
+    parser.add_argument('--verbose', '-v', action='count')
+#    parser.add_argument('-o', '--output_format', dest='output_format', help='output_format [ansi, ascii]',
+#        default="ansi")
+    args = parser.parse_args()
+
+#    if args.output_format == 'ansi':
+#        fmt = AnsiFormatter()
+#    else:
+#        fmt = BaseFormatter()
+
+    main()
